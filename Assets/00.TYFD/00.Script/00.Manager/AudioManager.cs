95ee47c0f3ad31f9b99356d74195d56153b1bc7b
kimtaeyun0419 <sdh230104@sdh.hs.kr>
2024-07-20 20:30:02 +0900

    ADD | AudioManager

---
+++ Assets/00.TYFD/00.Script/00.Manager/AudioManager.cs
@@ -0,0 +1,69 @@
+// # System
+using System.Collections;
+using System.Collections.Generic;
+
+// # Unity
+using UnityEngine;
+
+[System.Serializable]
+public class Sound
+{
+    public string clipName;
+    public AudioClip audioClip;
+}
+
+public class AudioManager : MonoBehaviour
+{
+    public static AudioManager instance;
+
+    [SerializeField] private List<Sound> bgms;
+    [SerializeField] private List<Sound> sfxs;
+
+    [SerializeField] private AudioSource bgmSource;
+    [SerializeField] private AudioSource sfxSource;
+
+    private void Awake()
+    {
+        if(instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(this);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public void PlayMusic(string name)
+    {
+        foreach(var sound in bgms)
+        {
+            if(name == sound.clipName)
+            {
+                bgmSource.clip = sound.audioClip;
+                bgmSource.Play();
+                break;
+            }
+            else
+            {
+                Debug.Log("음악을 찾을 수 없습니다.");
+            }
+        }
+    }
+
+    public void PlaySfx(string name)
+    {
+        foreach(var sound in sfxs)
+        {
+            if(name == sound.clipName)
+            {
+                sfxSource.PlayOneShot(sound.audioClip);
+            }
+            else
+            {
+                Debug.Log("효과음을 찾을 수 없습니다.");
+            }
+        }
+    }
+}
