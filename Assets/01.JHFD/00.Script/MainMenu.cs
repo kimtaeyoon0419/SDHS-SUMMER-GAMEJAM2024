bd52d021676ae991420e4c8693783456b87da455
janghyun1037 <hyunjhw@naver.com>
2024-07-20 15:32:15 +0900

    Title

---
+++ Assets/01.JHFD/00.Script/MainMenu.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenu : MonoBehaviour
+{
+    [SerializeField] private GameObject optionPanel;
+    [SerializeField] private bool isOptionPanel;
+    [SerializeField] private GameObject CreditPanel;
+    [SerializeField] private bool isCreditPanel;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnClickStart()
+    {
+        Debug.Log("시작하기");
+    }
+
+    public void OnClickOption()
+    {
+        isOptionPanel = !isOptionPanel;
+        optionPanel.SetActive(isOptionPanel);
+    }
+
+
+    public void OnClickCredit()
+    {
+        isCreditPanel = !isCreditPanel;
+        CreditPanel.SetActive(isCreditPanel);
+    }
+    public void OnClilckQuit()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else 
+        Application.Quit();
+#endif
+    }
+}
