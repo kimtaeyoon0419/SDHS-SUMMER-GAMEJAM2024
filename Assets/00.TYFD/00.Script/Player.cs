dd1dc5f54f132753748834c24259f85b3fa1e969
kimtaeyun0419 <sdh230104@sdh.hs.kr>
2024-07-20 13:15:05 +0900

    ADD | PlayerAttack

--- Assets/00.TYFD/00.Script/Player.cs
+++ Assets/00.TYFD/00.Script/Player.cs
@@ -1,8 +1,5 @@
-// # System
 using System.Collections;
 using System.Collections.Generic;
-
-// # Unity
 using UnityEngine;
 
 public class Player : MonoBehaviour
@@ -19,9 +16,17 @@ public class Player : MonoBehaviour
     [SerializeField] private float attackSpeed;
     [SerializeField] private float curAttackSpeed;
     [SerializeField] private float moveSpeed;
+    [SerializeField] private int attackCount;
 
     [Header("Animation")]
     private readonly int hashMove = Animator.StringToHash("Move");
+    private readonly int hashAttack1 = Animator.StringToHash("Attack1");
+    private readonly int hashAttack2 = Animator.StringToHash("Attack2");
+    private readonly int hashAttack3 = Animator.StringToHash("Attack3");
+
+    [Header("Attack")]
+    private bool isAttack = false;
+    [SerializeField] private Transform attackBoxPos;
 
     private void Awake()
     {
@@ -39,6 +44,11 @@ public class Player : MonoBehaviour
         float hor = Input.GetAxisRaw("Horizontal");
         Move(hor);
         Flip(hor);
+
+        if (Input.GetKeyDown(KeyCode.X))
+        {
+            Attack();
+        }
     }
 
     /// <summary>
@@ -47,6 +57,8 @@ public class Player : MonoBehaviour
     /// <param name="x">좌우 값</param>
     private void Move(float x)
     {
+        if (isAttack) return;  // 공격 중일 때는 움직이지 않음
+
         rb.velocity = new Vector2(x * moveSpeed, rb.velocity.y);
         if (x != 0)
         {
@@ -54,9 +66,10 @@ public class Player : MonoBehaviour
         }
         else
         {
-            animator.SetBool(hashMove,false);
+            animator.SetBool(hashMove, false);
         }
     }
+
     /// <summary>
     /// 방향전환
     /// </summary>
@@ -67,12 +80,62 @@ public class Player : MonoBehaviour
         {
             if (x < 0)
             {
-                transform.rotation = Quaternion.Euler(0, 180 * x, 0);
+                transform.rotation = Quaternion.Euler(0, 180, 0);
             }
-            else if(x > 0)
+            else if (x > 0)
             {
                 transform.rotation = Quaternion.Euler(0, 0, 0);
             }
         }
+    }
+
+    private void Attack()
+    {
+        if (isAttack)
+        {
+            return;
+        }
+
+        if (attackCount == 0)
+        {
+            animator.SetTrigger(hashAttack1);
+            attackCount++;
+            OnAttackBox();
+        }
+        else if (attackCount == 1)
+        {
+            animator.SetTrigger(hashAttack2);
+            attackCount++;
+            OnAttackBox();
+        }
+        else if (attackCount == 2)
+        {
+            animator.SetTrigger(hashAttack3);
+            attackCount++;
+            attackCount = 0;
+            OnAttackBox();
+        }
+        StartCoroutine(isAttacking());
+    }
+
+    private void OnAttackBox()
+    {
+        Collider2D[] curEnemy = Physics2D.OverlapBoxAll(attackBoxPos.position, new Vector2(1, 1), 0);
+        if (curEnemy != null)
+        {
+            foreach (Collider2D enemy in curEnemy)
+            {
+                enemy.GetComponent<Enemy>()?.TakeDamage(attackPower);
+            }
+        }
+    }
+
+    IEnumerator isAttacking()
+    {
+        isAttack = true;
+
+        yield return new WaitForSeconds(0.2f);
+
+        isAttack = false;
     }
 }
